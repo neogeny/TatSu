e760f4150339c0dbfd9bb28fbb33492cb58714d7
Juancarlo Añez <apalala@gmail.com>
2022-01-05 14:19:56 -0400

    [mixins][indent] handle multiple lines + formalize

--- tatsu/mixins/indent.py
+++ tatsu/mixins/indent.py
@@ -5,8 +5,21 @@ from contextlib import contextmanager
 
 
 class IndentPrintMixin:
-    def __init__(self):
+    def __init__(self, indent=4):
+        self._indent_amount = indent
         self._indent_level = 0
+
+    @property
+    def indent_amount(self):
+        return self._indent_amount
+
+    @property
+    def indent_level(self):
+        return self._indent_level
+
+    @property
+    def current_indentation(self):
+        return ' ' * self._indent_amount * self._indent_level
 
     @contextmanager
     def indent(self):
@@ -16,9 +29,13 @@ class IndentPrintMixin:
         finally:
             self._indent_level -= 1
 
+    def _do_print(self, line):
+        print((self.current_indentation + line).rstrip())
+
     def print(self, *args, **kwargs):
+        kwargs.pop('file', None)  # do not allow redirection of output
         with io.StringIO() as output:
             print(*args, file=output, **kwargs)
             text = output.getvalue()
-        indentation = ' ' * 4 * self._indent_level
-        print((indentation + text).rstrip())
+        for line in text.splitlines(keepends=True):
+            self._do_print(line)
