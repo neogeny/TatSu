9d55a1f956ef639c065ee680c1b4bb6af83ba7c1
Juancarlo Añez <apalala@gmail.com>
2022-01-24 22:59:36 -0400

    [mixins][indent] refactor mixin to prepare it for code generation

--- tatsu/mixins/indent.py
+++ tatsu/mixins/indent.py
@@ -13,9 +13,17 @@ class IndentPrintMixin:
         self.indent_level = 0
         self.output_stream = sys.stdout
 
-    @property
-    def current_indentation(self):
-        return ' ' * self.indent_amount * self.indent_level
+    def print(self, *args, **kwargs):
+        lines = self.as_printed_lines(*args, **kwargs)
+        self._do_print_lines(lines)
+
+    def as_printed(self, *args, **kwargs):
+        lines = self.as_printed_lines(*args, **kwargs)
+        return '\n'.join(lines)
+
+    def as_printed_lines(self, *args, **kwargs):
+        text = self.io_print(*args, **kwargs)
+        return self.indented_lines(text)
 
     @contextmanager
     def indent(self):
@@ -25,18 +33,29 @@ class IndentPrintMixin:
         finally:
             self.indent_level -= 1
 
-    def _do_print(self, line: str = ''):
-        print((self.current_indentation + line).rstrip(), file=self.output_stream)
+    @property
+    def current_indentation(self):
+        return ' ' * self.indent_amount * self.indent_level
 
-    def print(self, *args, **kwargs):
+    @staticmethod
+    def io_print(*args, **kwargs):
         kwargs.pop('file', None)  # do not allow redirection of output
         with io.StringIO() as output:
             print(*args, file=output, **kwargs)
             text = output.getvalue()
-        text = trim(text)
-        lines = text.splitlines(keepends=True)
+        return text
+
+    def _do_print_lines(self, lines: list[str] = None):
         if not lines:
-            self._do_print()
-        else:
-            for line in lines:
-                self._do_print(line)
+            print(file=self.output_stream)
+            return
+
+        for line in lines:
+            print(line, file=self.output_stream)
+
+    def indented_lines(self, text):
+        text = trim(text)
+        return [
+            (self.current_indentation + line).rstrip()
+            for line in text.splitlines(keepends=False)
+        ]
