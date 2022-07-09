5ba6b919a6cd5524dc744bda1c18fa64c834e454
Juancarlo Añez <apalala@gmail.com>
2022-07-09 10:32:46 -0400

    [walkers] ensure one cache per walker class (#275)
    fixes #266

--- test/walker_test.py
+++ test/walker_test.py
@@ -2,7 +2,7 @@ import json
 from collections import defaultdict
 
 import tatsu
-from tatsu.walkers import DepthFirstWalker
+from tatsu.walkers import NodeWalker, DepthFirstWalker
 from tatsu.util import asjson
 
 
@@ -41,3 +41,18 @@ def test_walk_node_ast():
     print(json.dumps(asjson(model), indent=2))
     PW().walk(model)
     assert seen == {'SampleExpression': 2, 'TemporalSeq': 1}
+
+
+def test_cache_per_class():
+    class PW(DepthFirstWalker):
+        pass
+
+    assert isinstance(NodeWalker._walker_cache, dict)  # pylint: disable=no-member
+    assert isinstance(DepthFirstWalker._walker_cache, dict)  # pylint: disable=no-member
+    assert isinstance(PW._walker_cache, dict)  # pylint: disable=no-member
+
+    assert id(NodeWalker._walker_cache) != id(DepthFirstWalker._walker_cache)  # pylint: disable=no-member
+    assert id(PW._walker_cache) != id(DepthFirstWalker._walker_cache)  # pylint: disable=no-member
+
+    walker = PW()  # pylint: disable=no-member
+    assert id(walker._walker_cache) == id(PW._walker_cache)  # pylint: disable=no-member
