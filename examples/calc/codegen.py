6e48596014d33683d29ce64af80ec1499dcd45f1
apalala <apalala@gmail.com>
2023-08-21 09:25:57 -0400

    [lint] solve warnings

--- examples/calc/codegen.py
+++ examples/calc/codegen.py
@@ -13,18 +13,15 @@ class PostfixCodeGenerator(NodeWalker, IndentPrintMixin):
     def walk_Number(self, node: Node):
         self.print(f'PUSH {self.walk(node.value)}')  # type: ignore
 
-
     def walk_Add(self, node: Node, *args, **kwargs):
         self.walk(node.left)  # type: ignore
         self.walk(node.right)  # type: ignore
         print('ADD')
 
-
     def walk_Subtract(self, node: Node, *args, **kwargs):
         self.walk(node.left)  # type: ignore
         self.walk(node.right)  # type: ignore
         print('SUB')
-
 
     def walk_Multiply(self, node: Node, *args, **kwargs):
         self.walk(node.left)  # type: ignore
@@ -36,5 +33,5 @@ class PostfixCodeGenerator(NodeWalker, IndentPrintMixin):
         self.walk(node.right)  # type: ignore
         print('DIV')
 
-    def walk_int(self, node:Node, *args, **kwargs):
+    def walk_int(self, node: Node, *args, **kwargs):
         print('PUSH', node)
