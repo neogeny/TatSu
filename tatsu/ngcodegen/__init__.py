caf280aa8d781f54e650a20139d677b16db7dcc0
apalala <apalala@gmail.com>
2023-11-28 19:34:04 -0400

    [ngcodegen] up to Buffer definition

--- tatsu/ngcodegen/__init__.py
+++ tatsu/ngcodegen/__init__.py
@@ -1,5 +1,9 @@
 from ..objectmodel import ParseModel
 
+from .python import PythonCodeGenerator
+
 
 def codegen(model: ParseModel) -> str:
-    return '** NG CODEGEN NOT IMPLEMENTED YET **'
+    generator = PythonCodeGenerator()
+    generator.walk(model)
+    return generator.printed_text()
