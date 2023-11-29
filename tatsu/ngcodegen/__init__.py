21cbaf991e269e83eceb119a1f7bb59e889ea908
apalala <apalala@gmail.com>
2023-11-28 21:43:00 -0400

    [ngcodegen] allow naming the parser

--- tatsu/ngcodegen/__init__.py
+++ tatsu/ngcodegen/__init__.py
@@ -2,7 +2,7 @@ from ..objectmodel import ParseModel
 from .python import PythonCodeGenerator
 
 
-def codegen(model: ParseModel) -> str:
-    generator = PythonCodeGenerator()
+def codegen(model: ParseModel, parser_name: str = '') -> str:
+    generator = PythonCodeGenerator(parser_name=parser_name)
     generator.walk(model)
     return generator.printed_text()
