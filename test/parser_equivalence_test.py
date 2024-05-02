cfd111cadd869d7041443a4251f680e8b9d545d7
apalala <apalala@gmail.com>
2024-05-02 10:32:18 -0400

    [test] add test for whitespace in generated parser

--- test/parser_equivalence_test.py
+++ test/parser_equivalence_test.py
@@ -42,7 +42,7 @@ GRAMMAR = r"""
 
 def generate_and_load_parser(name, grammar):
     code = tatsu.to_python_sourcecode(grammar, name='Test')
-    # print(code)
+    print(code)
     module = types.ModuleType(name)
     module.__file__ = '<generated>'
     exec(compile(code, module.__file__, 'exec'), module.__dict__)
@@ -167,3 +167,6 @@ def test_none_whitespace():
     output = parser.parse(input)
     assert output == ('This is a', ' test')
 
+    parser = generate_and_load_parser('W', grammar)
+    output = parser.parse(input, parseinfo=False)
+    assert output == ('This is a', ' test')
