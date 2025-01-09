e0c44a1cb31ac7edd84a701f2880f5fd8b37f483
apalala <apalala@gmail.com>
2025-01-09 14:03:41 -0400

    [infos][config] delete comments_re etc from ParserConfig class
    fixes # 356

--- tatsu/infos.py
+++ tatsu/infos.py
@@ -72,6 +72,8 @@ class ParserConfig:
             )
         del self.comments_re
         del self.eol_comments_re
+        del ParserConfig.comments_re
+        del ParserConfig.eol_comments_re
 
         if self.comments:
             cached_re_compile(self.comments)
