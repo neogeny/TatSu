1a82ca0245172c00f153351df773e575bd136fc6
apalala <apalala@gmail.com>
2026-01-15 19:08:10 -0400

    [walkers] restore ContextWalker for the benefit of existing projects

--- tatsu/walkers.py
+++ tatsu/walkers.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from collections.abc import Callable, Mapping
+from contextlib import contextmanager
 from typing import Any, ClassVar, Concatenate, cast
 
 from .ngmodel import nodeshell
@@ -132,3 +133,53 @@ class DepthFirstWalker(NodeWalker):
             return [self.walk(e, *args, **kwargs) for e in iter(node)]
         else:
             return super().walk(node, [], *args, **kwargs)
+
+
+class ContextWalker(NodeWalker):
+    def __init__(self, initial_context):
+        super().__init__()
+        self._initial_context = initial_context
+        self._context_stack = [initial_context]
+
+    # abstract
+    def get_node_context(self, node, *args, **kwargs):
+        pass
+
+    # abstract
+    def enter_context(self, ctx):
+        pass
+
+    # abstract
+    def leave_context(self, ctx):
+        pass
+
+    def push_context(self, ctx):
+        self._context_stack.append(ctx)
+
+    def pop_context(self):
+        self._context_stack.pop()
+
+    @property
+    def initial_context(self):
+        return self._initial_context
+
+    @property
+    def context(self):
+        return self._context_stack[-1]
+
+    @contextmanager
+    def new_context(self, node, *args, **kwargs):
+        node = nodeshell(node)
+        ctx = self.get_node_context(node, *args, **kwargs)
+        if ctx == self.context:
+            yield ctx
+        else:
+            self.enter_context(ctx)
+            try:
+                self.push_context(ctx)
+                try:
+                    yield ctx
+                finally:
+                    self.pop_context()
+            finally:
+                self.leave_context(ctx)
