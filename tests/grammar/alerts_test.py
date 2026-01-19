9b0656a32e004a8b0d46ae9d4b0da277d15596cb
apalala <apalala@gmail.com>
2026-01-19 18:30:25 -0400

    [tests] add debugging output

--- tests/grammar/alerts_test.py
+++ tests/grammar/alerts_test.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
 
 from tatsu.tool import compile
+from tatsu.util import asjsons
 
 
-def test_alert_interpolation():
+def test_alert_interpolation(trace=True):
     input = '42 69'
     grammar = r"""
             start = a:number b: number i:^`"seen: {a}, {b}"` $ ;
             number = /\d+/ ;
     """
     model = compile(grammar)
-    print(model)
-    ast = model.parse(input)
+    print(asjsons(model))
+    ast = model.parse(input, trace=trace)
     assert ast == {'a': '42', 'b': '69', 'i': 'seen: 42, 69'}
