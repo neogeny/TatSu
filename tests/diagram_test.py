1df0bbd984ea09cc23c1a57dc7a2ba31d576c7ad
apalala <apalala@gmail.com>
2026-01-21 20:50:43 -0400

    [tests] set diagram_test to write a .svg

--- tests/diagram_test.py
+++ tests/diagram_test.py
@@ -1,29 +1,14 @@
-import unittest
-
 from tatsu.tool import compile
 
 
-class DiagramTests(unittest.TestCase):
-    def test_dot(self):
-        grammar = """
-            start = "foo\\nbar" $;
-        """
-        try:
-            from tatsu.diagrams import draw
-        except ImportError:
-            return
+def test_dot():
+    grammar = """
+        start = "foo\\nbar" $;
+    """
+    try:
+        from tatsu.diagrams import draw
+    except ImportError:
+        return
 
-        m = compile(grammar, 'Diagram')
-        draw('tmp/diagram.png', m)
-
-
-def suite():
-    return unittest.TestLoader().loadTestsFromTestCase(DiagramTests)
-
-
-def main():
-    unittest.TextTestRunner(verbosity=2).run(suite())
-
-
-if __name__ == '__main__':
-    main()
+    m = compile(grammar, 'Diagram')
+    draw('tmp/diagram.svg', m)
