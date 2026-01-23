d9e1500c4399a0ae5b825fd0323c0e87c2357cec
apalala <apalala@gmail.com>
2026-01-23 18:18:18 -0400

    [model] drop structural equality / hide implementation __atributes

--- tatsu/objectmodel.py
+++ tatsu/objectmodel.py
@@ -17,16 +17,19 @@ __all__ = ['BaseNode', 'Node']
 class BaseNode(AsJSONMixin):
     # NOTE: declare at the class level in case __init__ is not called
     ast: Any = None
-    _attributes: dict[str, Any] = {}  # noqa: RUF012
+    __attributes: dict[str, Any] = {}  # noqa: RUF012
 
     def __init__(self, ast: Any = None, **kwargs: Any):
         super().__init__()
         self.ast: Any = ast
-        self._attributes: dict[str, Any] = {}
+        self.__attributes: dict[str, Any] = {}
 
         # NOTE: the old objectmodel.Node would addd new attributes to self
         allargs = ast | kwargs if isinstance(self.ast, AST) else kwargs
-        for name, value in allargs.items():
+        self.__set_attributes(**allargs)
+
+    def __set_attributes(self, **attrs) -> None:
+        for name, value in attrs.items():
             if hasattr(self, name) and not inspect.ismethod(getattr(self, name)):
                 setattr(self, name, value)
             else:
@@ -36,13 +39,13 @@ class BaseNode(AsJSONMixin):
                         f' {type(self).__name__}.{name}',
                         stacklevel=2,
                     )
-                self._attributes[name] = value
+                self.__attributes[name] = value
 
     def __getattr__(self, name: str) -> Any:
         # note: here only if normal attribute search failed
         try:
-            assert isinstance(self._attributes, dict)
-            return self._attributes[name]
+            assert isinstance(self.__attributes, dict)
+            return self.__attributes[name]
         except KeyError:
             return super().__getattribute__(name)
 
@@ -56,32 +59,19 @@ class BaseNode(AsJSONMixin):
         )
 
     def __eq__(self, other) -> bool:
-        if id(self) == id(other):
-            return True
-        elif self.ast is None:
-            return False
-        elif not getattr(other, 'ast', None):
-            return False
-        else:
-            return self.ast == other.ast
+        # note: no use case for structural equality
+        return id(self) == id(other)
 
     def __hash__(self) -> int:
-        if self.ast is None:
-            return id(self)
-        elif isinstance(self.ast, list):
-            return hash(tuple(self.ast))
-        elif isinstance(self.ast, dict):
-            return hash(AST(self.ast))
-        else:
-            return hash(self.ast)
+        return hash(id(self))
 
     def __getstate__(self) -> Any:
-        return self._nonrefdict()
+        return self.__nonrefdict()
 
     def __setstate__(self, state):
         self.__dict__.update(state)
 
-    def _nonrefdict(self) -> Mapping[str, Any]:
+    def __nonrefdict(self) -> Mapping[str, Any]:
         return {
             name: value if (
                     type(value) not in {weakref.ReferenceType, weakref.ProxyType}
@@ -95,21 +85,21 @@ class Node(BaseNode):
     ctx: Any = None
     parseinfo: ParseInfo | None = None
 
-    _parent_ref: weakref.ref | None = None
+    __parent_ref: weakref.ref | None = None
 
     def __init__(self, ast: Any = None, ctx: Any = None, **kwargs: Any):
         super().__init__(ast=ast, **kwargs)
         self.ast: Any = ast
         self.ctx: Any = ctx
         self.parseinfo: ParseInfo | None = None
-        self._parent_ref: weakref.ref[Node] | None = None
+        self.__parent_ref: weakref.ref[Node] | None = None
 
     def set_parseinfo(self, value: ParseInfo | None) -> None:
         self.parseinfo = value
 
     @property
     def parent(self) -> Node | None:
-        ref = self._parent_ref
+        ref = self.__parent_ref
         if ref is None:
             return None
         else:
@@ -149,10 +139,10 @@ class Node(BaseNode):
         return list(self.children())
 
     def children(self) -> Iterable[Any]:
-        def walk(obj: Any) -> Iterable[Node]:
+        def dfs(obj: Any) -> Iterable[Node]:
             match obj:
                 case Node() as node:
-                    node._parent_ref = weakref.ref(self)
+                    node.__parent_ref = weakref.ref(self)
                     yield node
                 case Mapping() as map:
                     for name, value in map.items():
@@ -160,11 +150,11 @@ class Node(BaseNode):
                             continue
                         if value is None:
                             continue
-                        yield from walk(value)
+                        yield from dfs(value)
                 case (list() | tuple()) as seq:
                     for item in seq:
-                        yield from walk(item)
+                        yield from dfs(item)
                 case _:
                     pass
 
-        return tuple(walk(self._pubdict()))
+        return tuple(dfs(self._pubdict()))
