a37fc00a2c9f806b58d459f8c59f0490116917ff
apalala <apalala@gmail.com>
2026-02-01 13:29:14 -0400

    [config] deprecate comments_re and eol_comments_re SOFTLY

--- tatsu/parserconfig.py
+++ tatsu/parserconfig.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import re
+import warnings
 from collections.abc import Collection
 from dataclasses import dataclass, field
 from typing import Any, override
@@ -23,9 +24,6 @@ class ParserConfig(Config):
     start: str | None = None  # FIXME
     start_rule: str | None = None  # FIXME
     rule_name: str | None = None  # Backward compatibility
-
-    comments_re: re.Pattern | str | None = None  # WARNING: deprecated
-    eol_comments_re: re.Pattern | str | None = None  # WARNING: deprecated
 
     tokenizercls: type[Tokenizer] = NullTokenizer
     semantics: Any = None
@@ -52,7 +50,7 @@ class ParserConfig(Config):
     keywords: Collection[str] = field(default_factory=set)
 
     ignorecase: bool = False
-    namechars: str = ''
+    namechars: str | None = None
     nameguard: bool | None = None  # implied by namechars
     whitespace: str | None = None
     whitespace: str | None = Undefined  # type: ignore
@@ -61,26 +59,13 @@ class ParserConfig(Config):
     # deprecated: some old projects use these
     owner: Any = None
     extra: Any = None
+    comments_re: re.Pattern | str | None = None  # WARNING: deprecated
+    eol_comments_re: re.Pattern | str | None = None  # WARNING: deprecated
 
     def __post_init__(self):  # pylint: disable=W0235
         if self.ignorecase:
             self.keywords = {k.upper() for k in self.keywords}
         super().__post_init__()
-        if self.comments_re or self.eol_comments_re:
-            raise AttributeError(
-                "Both `comments_re` and `eol_comments_re` "
-                "have been removed from parser configuration. "
-                "Please use `comments` and/or `eol_comments` instead`.",
-            )
-        del self.comments_re
-        del self.eol_comments_re
-
-        if self.comments:
-            cached_re_compile(self.comments)
-        if self.eol_comments:
-            cached_re_compile(self.eol_comments)
-        if self.whitespace:
-            cached_re_compile(self.whitespace)
 
         if not self.memoization:
             self.left_recursion = False
@@ -93,6 +78,35 @@ class ParserConfig(Config):
                 f'semantics must be an object instance or None, not class {self.semantics!r}',
             )
 
+        self._deprecate_and_compile_comments()
+
+    def _deprecate_and_compile_comments(self):
+        # note: handle deprecations gracefully
+        if self.comments_re:
+            warnings.warn(
+                'ParserConfig.comments_re is deprecated: use `comments`',
+                stacklevel=3,
+            )
+            if not self.comments:
+                self.comments = str(self.comments_re)
+            del self.comments_re
+
+        if self.eol_comments_re:
+            warnings.warn(
+                'ParserConfig.eol_comments_re is deprecated: use `eol_comments`',
+                stacklevel=3,
+            )
+            if not self.eol_comments:
+                self.eol_comments = str(self.eol_comments_re)
+            del self.eol_comments_re
+
+        if self.comments and isinstance(self.comments, str):
+            cached_re_compile(self.comments)
+        if self.eol_comments and not isinstance(self.eol_comments, re.Pattern):
+            cached_re_compile(self.eol_comments)
+        if self.whitespace and not isinstance(self.whitespace, re.Pattern):
+            cached_re_compile(self.whitespace)
+
     def effective_rule_name(self):
         # note: there are legacy reasons for this mess
         return self.start_rule or self.rule_name or self.start
@@ -102,4 +116,5 @@ class ParserConfig(Config):
         result = super().override(**settings)
         if 'grammar' in settings:
             result.name = result.grammar
+        self._deprecate_and_compile_comments()
         return result
