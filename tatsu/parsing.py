65e32da44c8e172600751fbecf76b41aa5ded12c
apalala <apalala@gmail.com>
2026-02-02 10:05:23 -0400

    [parsing] improve typing and adopt generic_main()

--- tatsu/parsing.py
+++ tatsu/parsing.py
@@ -1,33 +1,32 @@
 from __future__ import annotations
 
 import inspect
-from collections.abc import Callable
+import sys
+from types import MethodType
 
 from .contexts import (
     ParseContext,
     isname,
     leftrec,
     nomemo,
-    tatsumasu,
+    竜rule,
 )
 from .exceptions import FailedRef
 
-__all__ = ['Parser', 'isname', 'leftrec', 'nomemo', 'tatsumasu']
+__all__ = ['Parser', 'generic_main', 'isname', 'leftrec', 'nomemo', '竜rule']
 
 
 class Parser(ParseContext):
-    def _find_rule(self, name: str) -> Callable:
-        rule = getattr(self, '_' + name + '_', None)
-        if isinstance(rule, type(self._find_rule)):
-            return rule
-        rule = getattr(self, name, None)
-        if isinstance(rule, type(self._find_rule)):
-            return rule
+    def _find_rule(self, name: str) -> MethodType:
+        for rulename in (f'_{name}_', name):
+            rule = getattr(self, rulename, None)
+            if inspect.ismethod(rule):
+                return rule
         return self._error(name, exclass=FailedRef)
 
     @classmethod
     def rule_list(cls) -> list[str]:
-        methods = inspect.getmembers(cls, predicate=inspect.isroutine)
+        methods = inspect.getmembers(cls, predicate=inspect.ismethod)
         result = []
         for m in methods:
             name = m[0]
@@ -38,3 +37,76 @@ class Parser(ParseContext):
             if name.startswith('_') and name.endswith('_'):
                 result.append(name[1:-1])
         return result
+
+
+def generic_main(custom_main, parser_class, name='Unknown'):
+    import argparse
+
+    class ListRules(argparse.Action):
+        def __call__(self, parser, namespace, values, option_string=None):
+            print('Rules:')
+            for r in parser_class.rule_list():
+                print(r)
+            print()
+            sys.exit(0)
+
+    argp = argparse.ArgumentParser(description=f'Simple parser for {name}.')
+    addarg = argp.add_argument
+
+    addarg(
+        '-c',
+        '--color',
+        help='use color in traces (requires the colorama library)',
+        action='store_true',
+    )
+    addarg(
+        '-l',
+        '--list',
+        action=ListRules,
+        nargs=0,
+        help='list all rules and exit',
+    )
+    addarg(
+        '-n',
+        '--no-nameguard',
+        action='store_true',
+        dest='no_nameguard',
+        help="disable the 'nameguard' feature",
+    )
+    addarg(
+        '-t', '--trace', action='store_true', help='output trace information',
+    )
+    addarg(
+        '-w',
+        '--whitespace',
+        type=str,
+        default=None,
+        help='whitespace specification',
+    )
+    addarg(
+        'file',
+        metavar='FILE',
+        help="the input file to parse or '-' for standard input",
+        nargs='?',
+        default='-',
+    )
+    addarg(
+        'startrule',
+        metavar='STARTRULE',
+        nargs='?',
+        help='the start rule for parsing',
+        default=None,
+    )
+
+    args = argp.parse_args()
+    try:
+        return custom_main(
+            args.file,
+            start=args.startrule,
+            trace=args.trace,
+            whitespace=args.whitespace,
+            nameguard=not args.no_nameguard,
+            colorize=args.color,
+        )
+    except KeyboardInterrupt:
+        pass
