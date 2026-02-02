e609b96819d0be76cc88e772bade6033c03f82c1
apalala <apalala@gmail.com>
2026-02-02 01:41:30 -0400

    [tests] minor cleanup

--- tests/model_test.py
+++ tests/model_test.py
@@ -34,7 +34,10 @@ def test_node_kwargs() -> None:
     atom = Atom(ast={'bar': 1}, symbol='foo')
     assert atom.ast == {'bar': 1}
 
-    with pytest.warns(UserWarning, match='"children" in keyword arguments will shadow Atom.children'):
+    with pytest.warns(
+            UserWarning,
+            match=r'children.*?in keyword arguments will shadow.*?Atom\.children'
+    ):
         atom = Atom(children=[])
         assert atom
 
