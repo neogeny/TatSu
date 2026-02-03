6aad10afbe180e57a805447e726471bcc1521084
apalala <apalala@gmail.com>
2026-02-03 09:44:11 -0400

    [util] sketch help for version identifiers and tuples

---
+++ tatsu/util/version.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2017-2026 Juancarlo Añez (apalala@gmail.com)
+# SPDX-License-Identifier: BSD-4-Clause
+
+# NOTE
+#   PEP 440 – Version Identification and Dependency Specification
+#   https://peps.python.org/pep-0440/
+#   https://github.com/pypa/packaging
+
+from __future__ import annotations
