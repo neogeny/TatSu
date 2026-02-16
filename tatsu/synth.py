ad9bc7a66d34c8accea21df29d4ed754a2c39f6e
apalala <apalala@gmail.com>
2026-02-16 15:08:09 -0400

    [synth] use types.new_class for synthetic types

--- tatsu/synth.py
+++ tatsu/synth.py
@@ -2,18 +2,18 @@
 # SPDX-License-Identifier: BSD-4-Clause
 from __future__ import annotations
 
-from collections.abc import Callable, MutableMapping
+import types
 from typing import Any
 
 from .ast import AST
 from .objectmodel import BaseNode
 
+__all__ = ['SynthNode', 'registered_synthetics', 'synthesize']
+
 # NOTE:
 #   __registry is an alias for the modules vars()/ __dict__.
 #   This allows for synthesized types to reside within this module.
-__registry: MutableMapping[str, Callable] = vars()
-
-__all__ = ['SynthNode', 'registered_synthetics', 'synthesize']
+__registry: dict[str, Any] = vars()
 
 
 class SynthNode(BaseNode):
@@ -30,20 +30,34 @@ class SynthNode(BaseNode):
         self.ast = None
 
 
-def synthesize(name: str, bases: tuple[type, ...], **kwargs: Any) -> Callable:
+def synthesize(name: str, bases: tuple[type, ...], **kwargs: Any) -> type:
+    # by Apalala 2026/02/16 <- 2017
+    # by Gemini  2026/02/16
     if not isinstance(bases, tuple):
         raise TypeError(f'bases must be a tuple, not {type(bases)}')
 
     if SynthNode not in bases:
         bases = (*bases, SynthNode)
 
-    constructor = __registry.get(name)
-    if not constructor:
-        constructor = type(name, bases, {"__module__": __name__} | kwargs)
-        __registry[name] = constructor
+    found = __registry.get(name)
+    if isinstance(found, type):
+        return found
+    elif found:
+        raise TypeError(f'Found {name!r} in context but its type is {type(found)!r}')
 
-    return constructor
+    def build_body(ns: dict[str, Any]) -> None:
+        ns.update({"__module__": __name__})
+        ns.update(kwargs)
+
+    newcls: type = types.new_class(name, bases, exec_body=build_body)
+    __registry[name] = newcls
+
+    return newcls
 
 
-def registered_synthetics() -> dict[str, Callable]:
-    return dict(__registry)
+def registered_synthetics() -> dict[str, SynthNode]:
+    return {
+        name: value
+        for name, value in __registry.items()
+        if isinstance(value, SynthNode)
+    }
