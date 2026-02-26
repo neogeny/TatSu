a30cbbfcf1e0e32cc52c9a4fe56dc1836683a123
apalala <apalala@gmail.com>
2026-02-26 12:52:16 -0400

    [contexts] nothing changed

--- tatsu/contexts/decorator.py
+++ tatsu/contexts/decorator.py
@@ -9,7 +9,7 @@ from typing import Any, cast
 from .protocol import ParseCtx
 
 from .infos import RuleInfo, RuleLike
-from ..util import debug
+from ..util import debug  # noqa
 
 
 # note: decorator
@@ -41,37 +41,77 @@ def name(impl: Callable) -> Callable:
     return impl
 
 
+def fn(func: Callable) -> str:
+    return getattr(func, '__name__', '<?>')
+
+def tn(obj: Any) -> str:
+    return getattr(type(obj), '__name__', '.?.')
+
+
 # note: decorator
 class rule:
     def __init__(self, *args: Any, **kwargs: Any) -> None:
-        # allways used as rule()
-        if not __debug__:
-            raise RuntimeError('WANT DEBUG')
-        debug(f'__init__ {args=!r} {kwargs=!r}')
+        if func := kwargs.pop('func', None):
+            self.func = func
+        else:
+            self.func = None
+        # debug(f'__init__ {fn(self.func)!r} {args=!r} {kwargs=!r}')
         self.obj = None
-        self.func = None
         self.params = args
         self.kwparams = kwargs
         self.owner = None
 
-    def __get__(self, obj: Any, owner: Any = None) -> Any:
-        debug(f'__get__ {obj=!r} {owner=!r}')
-        self.obj = owner or obj
-        return self
+    def __get__(self, instance: Any, owner: Any = None) -> Any:
+        self.obj = instance
 
-    def __call__(self, func: Callable) -> Any:
-        debug(f'__call__ {self.obj=} {func=}')
-        self.func = func
+        debug(
+            f'__get__ {fn(self.func)=!r} {instance=!r} {owner=!r}'
+            f'  {tn(self.obj)=!r} '
+        )
+        if issubclass(owner, ParseCtx) and isinstance(instance, ParseCtx):
+            debug(f'__get__ LEGACY {instance=!r} {owner=!r}')
+            @functools.wraps(self.func)
+            def wrapper(_ctx: Any = None) -> Any:
+                debug(
+                    f'__legacy_wrapper__@__get__' 
+                    f' {fn(self.func)!r} {_ctx=!r} {instance=!r} {fn(owner)=!r}'
+                )
+                assert isinstance(instance, ParseCtx)
+                ruleinfo = RuleInfo.new(instance, self.func, self.params, self.kwparams)
+                return instance._call(ruleinfo)
+
+            return wrapper
+
+        @functools.wraps(self.func)
+        def wrapper(ctx: ParseCtx) -> Any:
+            debug(
+                f'__wrapper__@__get__ {fn(self.func)!r} {instance=!r} {tn(ctx)=!r}' 
+                f' {fn(instance)=!r} {fn(owner)!r}'
+            )
+            ruleinfo = RuleInfo.new(self.obj, self.func, self.params, self.kwparams)
+            assert isinstance(ctx, ParseCtx)
+            return ctx._call(ruleinfo)
+
+        return wrapper
+
+    def __call__(self, *args, **kwargs) -> Any:
+        # debug(f'__call__ {self.obj=} {args=} {kwargs=}')
+        func = args[0] if args else None
+        if not self.func:
+            self.func = func
+        if not self.obj:
+            other = rule(*self.params, func=func, **self.kwparams)
+            other.func = func
+            return other
 
         @functools.wraps(func)
-        def wrapper(obj: Any, ctx: ParseCtx | None = None) -> Any:
-            ruleinfo = RuleInfo.new(obj, func, self.params, self.kwparams)
-            debug(f'__wrapper__ {obj=!r} {ctx=!r} {ruleinfo=!r}')
+        def wrapper(ctx: ParseCtx | None = None) -> Any:
+            ruleinfo = RuleInfo.new(self.obj, func, self.params, self.kwparams)
+            # debug(f'__wrapper__@__call__ {tn(self.obj)=!r} {fn(ctx)=!r} {ruleinfo=!r}')
             if isinstance(ctx, ParseCtx):
                 return ctx._call(ruleinfo)
-
-            # note:
-            #   methods for rules used to be declared in the parser object
-            return obj._call(ruleinfo)  # legacy case
+            else:
+                # note: methods for rules used to be declared in the parser object
+                return obj._call(ruleinfo)
 
         return wrapper
