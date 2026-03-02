c2cffddf96ca7ce4f3ebe20a8bfd2f66a879fe23
apalala <apalala@gmail.com>
2026-03-02 15:04:33 -0400

    [grammars.patterm] use underscore for attrs not wanted in __repr__

--- tatsu/grammars/pattern.py
+++ tatsu/grammars/pattern.py
@@ -13,28 +13,28 @@ from tatsu.objectmodel import tatsudataclass
 @tatsudataclass
 class Pattern(Model):
     pattern: str = ''
-    patterns: list[str] = dc.field(init=False, default_factory=list)
-    regex: Pattern | None = dc.field(init=False, default=None)
+    _patterns: list[str] = dc.field(init=False, default_factory=list)
+    _regex: Pattern | None = dc.field(init=False, default=None)
 
     def __post_init__(self):
         super().__post_init__()
-        self.patterns = self.ast if isinstance(self.ast, list) else [self.ast]
-        self.pattern = ''.join(self.patterns)
-        self.regex = re.compile(self.pattern)
+        self._patterns = self.ast if isinstance(self.ast, list) else [self.ast]
+        self.pattern = ''.join(self._patterns)
+        self._regex = re.compile(self.pattern)
 
     def _parse(self, ctx):
         return ctx._pattern(self.pattern)
 
     def _first(self, k, f) -> ffset:
         x = str(self)
-        if bool(self.regex.match('')):
+        if bool(self._regex.match('')):
             return {(), (x,)}
         else:
             return {(x,)}
 
     def _pretty(self, lean=False):
         parts = []
-        for pat in (str(p) for p in self.patterns):
+        for pat in (str(p) for p in self._patterns):
             if '/' in pat:
                 newpat = pat.replace('"', r'\"')
                 regex = f'?"{newpat}"'
@@ -44,7 +44,7 @@ class Pattern(Model):
         return ' + '.join(parts)
 
     def _nullable(self) -> bool:
-        return bool(self.regex.match(''))
+        return bool(self._regex.match(''))
 
     def __str__(self):
         return self.pattern
