e36c390366921930227119734c10f282fac5a470
apalala <apalala@gmail.com>
2026-03-12 07:52:28 -0400

    [git] run black as part of pre-commit

--- scripts/git/hooks/common.py
+++ scripts/git/hooks/common.py
@@ -6,17 +6,34 @@ import subprocess
 from pathlib import Path
 
 
+def black(filenames: list[str]) -> list[str]:
+    """
+    Apply black to a list of filenames
+    """
+    try:
+        subprocess.run(  # noqa: S603
+            ['black', *filenames],  # noqa: S607
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+        return filenames
+    except subprocess.CalledProcessError:
+        return []
+
+
 def get_staged_files() -> list[Path]:
     """
     Get the list of files currently staged for commit.
     """
     try:
         result = subprocess.run(
-            ['git', 'diff', '--cached', '--name-only', '--diff-filter=d'],
+            ['git', 'diff', '--cached', '--name-only', '--diff-filter=d'],  # noqa: S607
             capture_output=True,
             text=True,
             check=True,
         )
-        return [Path(filename) for filename in result.stdout.splitlines()]
+        filenames = black(result.stdout.splitlines())
+        return [Path(filename) for filename in filenames]
     except subprocess.CalledProcessError:
         return []
