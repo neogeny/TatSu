4727e907be8f2e278384fbd0cd98dd28a6e08d17
apalala <apalala@gmail.com>
2026-03-17 10:55:04 -0400

    [ng] more play with forward declarations

--- ng/forward.py
+++ ng/forward.py
@@ -1,17 +1,16 @@
 # Copyright (c) 2017-2026 Juancarlo Añez (apalala@gmail.com)
 # SPDX-License-Identifier: BSD-4-Clause
-from __future__ import annotations
-
-from collections.abc import Callable
 
 
-def forward[T: Callable](fun: T) -> T:
-    return fun
+def forward[T](subject: T) -> T:
+    return subject
 
-
-# noinspection PyOverloads
+@forward
 def f() -> int: ...  # pyright: ignore[reportRedeclaration]  # ty:ignore[empty-body]
 
+assert f() is None
 
-def f() -> int:  # noqa: F811
+def f() -> int:
     return 0
+
+assert f() == 0
