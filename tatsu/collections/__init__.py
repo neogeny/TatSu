cfe66257f9457e1adafba35590559e8c9893be36
apalala <apalala@gmail.com>
2026-03-20 16:22:02 -0400

    [boundeddict] removed the collections package

--- tatsu/collections/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2017-2026 Juancarlo Añez (apalala@gmail.com)
-# SPDX-License-Identifier: BSD-4-Clause
-from __future__ import annotations
-
-from .boundeddict import BoundedDict
-from .lrudict import LRUBoundedDict
-
-__all__: list[str] = ['BoundedDict', 'LRUBoundedDict']
