0bde2ac65f84344551ee7bd8993ae8a0724cd0e5
apalala <apalala@gmail.com>
2026-06-24 11:50:53 -0400

    [util.debugging] clean up to make usable

--- tatsu/util/debugging.py
+++ tatsu/util/debugging.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: BSD-4-Clause
 from __future__ import annotations
 
+import logging
 import os
 import sys
 import warnings
@@ -10,7 +11,26 @@ from typing import Any
 from .strtools import prints
 
 
-TATSUDEBUG: int | str | None = os.environ.get('TATSUDEBUG', 0)
+def TATSUDEBUG() -> int:
+    return 10 * int(os.environ.get('TATSUDEBUG', logging.ERROR // 10))
+
+
+LEVEL: int = TATSUDEBUG()
+
+
+def set_debugging(level: int | str | None) -> None:
+    global LEVEL  # noqa: PLW0603
+    LEVEL = translate_level(level)
+
+
+def translate_level(level: int | str | None) -> int:
+    if isinstance(level, str):
+        return getattr(logging, level.upper())
+    elif isinstance(level, int):
+        if level < 10:
+            return level * 10
+        return level
+    return logging.ERROR
 
 
 def eprint(*args: Any, **kwargs: Any) -> None:
@@ -20,19 +40,25 @@ def eprint(*args: Any, **kwargs: Any) -> None:
     print(*args, file=file, **kwargs)
 
 
-def info(*args: Any, **kwargs: Any) -> None:
-    eprint('ⓘ', *args, **kwargs)
-
-
-def debug(*args: Any, **kwargs: Any) -> None:
-    if not (__debug__ or TATSUDEBUG):
+def DEBUG_print(*args: Any, **kwargs: Any) -> None:
+    if not (__debug__ or LEVEL > logging.DEBUG):
         return
     eprint(*args, **kwargs)
 
 
-def warning(*args: Any, **kwargs: Any) -> None:
+def INFO_print(*args: Any, **kwargs: Any) -> None:
+    if LEVEL > logging.INFO:
+        return
+    eprint('ⓘ', *args, **kwargs)
+
+
+def WARNING_print(*args: Any, **kwargs: Any) -> None:
+    if LEVEL > logging.WARNING:
+        return
+    eprint('⚠', *args, **kwargs)
     warnings.warn(prints(*args, **kwargs), stacklevel=2)
 
 
-def error(*args: Any, **kwargs: Any) -> None:
+def ERROR_print(*args: Any, **kwargs: Any) -> None:
+    WARNING_print(*args, **kwargs)
     raise RuntimeError(prints(*args, **kwargs))
